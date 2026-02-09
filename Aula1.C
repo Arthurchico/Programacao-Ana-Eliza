ec690bcc95bbd3dd58126be32604a17bbf71e806
Arthurchico <arthurchico10@gmail.com>
2026-02-09 12:14:17 +0000

    salveee

---
+++ Aula1.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    float altura;
+    char genero;
+    printf("Ola mundo \n");
+    printf("Qual a sua idade ?");
+    scanf("%d , &idade");
+    scanf ("%f");
+
+
+
+
+    printf("Ola mundo");
+
+    return 0;
+}
