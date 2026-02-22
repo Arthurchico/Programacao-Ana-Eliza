2181f6dec20022f829d57494519caf670c61c9fb
Arthurchico <arthurchico10@gmail.com>
2026-02-22 20:09:23 +0000

    Primeiraatv

--- Aula1.C
+++ Aula1.C
@@ -4,15 +4,26 @@ int main() {
     int idade;
     float altura;
     char genero;
-    printf("Ola mundo \n");
-    printf("Qual a sua idade ?");
-    scanf("%d , &idade");
-    scanf ("%f");
 
+    printf("Ola mundo!\n");
 
+                    
+    printf("Qual a sua idade? ");
+    scanf("%d", &idade); 
 
+                                
+    printf("Qual a sua altura? ");
+    scanf("%f", &altura);
 
-    printf("Ola mundo");
+                                            
+    printf("Qual o seu genero (M/F)? ");
+    scanf(" %c", &genero);
+
+                                                        
+    printf("\n--- Dados Cadastrados ---\n");
+    printf("Idade: %d anos\n", idade);
+    printf("Altura: %.2f metros\n", altura);
+    printf("Genero: %c\n", genero);
 
     return 0;
-}
+    }
